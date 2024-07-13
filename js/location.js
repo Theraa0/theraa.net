70ce7f744c9fa413177fd86132473da9b6495ad8
Rasmus <rasmus.metzger@gmail.com>
2024-07-13 22:35:27 +0530

    reworked page to run on astro

--- js/location.js
+++
@@ -1,4 +0,0 @@
-var activeTab = window.location.pathname;
-activeTab = activeTab.replace("/", "").replace(".html", "");
-document.getElementById(activeTab).classList.add("active");
-document.getElementById("pagetitle").innerText = activeTab;
