ba36d7e28694c2707092c23cec64de35f56fa411
Rasmus <rasmus.metzger@gmail.com>
2024-07-14 14:22:54 +0530

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://theraa.net",
-	integrations: [sitemap()]
+  site: "https://theraa.net",
+  integrations: [sitemap()],
+  output: "static",
+  adapter: vercelStatic()
 });
